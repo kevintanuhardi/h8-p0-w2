da15b00730dfc5794d9cbf721ca0ec0848e80b41
Kevin Wijaya <Kevin@Fernandas-MBP.localdomain>
2018-10-01 13:36:53 +0700

    adding ex 1 revised and split ext 7

--- excercise-7.js
+++ excercise-7.js
@@ -34,35 +34,3 @@ for(var i=1 ; i<=row ; i++){
   console.log(star);
   
 }
-
-// //Added Problem 1
-console.log("===========");
-var myNum = "";
-
-for(var j= 0 ; j < row; j++){
-
-  myNum = myNum + j;
-  for(var k = 0 ; k < row ; k++){
-    console.log(myNum);
-  }
-}
-
-//Added Problem 2
-console.log("===========");
-
- for(var i = 0 ; i < row; i++){
-   hash = "";
-   space = "";
-   for(var j = 0; j<row; j++){
-     if((j == 0 || j == row - 1 )||(i == 0 || i == row - 1)){
-       hash = hash + "#";
-     }else if(i == (row-1)/2 && j == (row-1)/2){
-       hash = hash + "*";
-     } 
-     else{ 
-       hash = hash + " ";
-       }
-     }
-   console.log(hash);
- }
-
